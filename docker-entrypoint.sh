15de6868959aa14b731f8f0b81ca82c2e6ad64f0
Ryan Harper <ryan.harper@pearson.com>
2015-05-20 23:22:46 +0100

    postgres tweaks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
+set -x
+echo $1
+echo $PGDATA
 
 if [ "$1" = 'postgres' ]; then
-	chown -R postgres "$PGDATA"
+ chown -R postgres "$PGDATA"
 	
 	chmod g+s /run/postgresql
 	chown -R postgres:postgres /run/postgresql
 	
 	if [ -z "$(ls -A "$PGDATA")" ]; then
-		gosu postgres initdb
+		gosu postgres initdb -A trust -U postgres
 		
 		sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
 		
@@ -36,31 +39,50 @@ if [ "$1" = 'postgres' ]; then
 			pass=
 			authMethod=trust
 		fi
-		
-		: ${POSTGRES_USER:=postgres}
+
+		: ${POSTGRES_USER:=artifactory}
 		: ${POSTGRES_DB:=$POSTGRES_USER}
 
-		if [ "$POSTGRES_DB" != 'postgres' ]; then
-			gosu postgres postgres --single -jE <<-EOSQL
-				CREATE DATABASE "$POSTGRES_DB" ;
-			EOSQL
-			echo
-		fi
-		
-		if [ "$POSTGRES_USER" = 'postgres' ]; then
+	
+        if [ "$POSTGRES_USER" = 'postgres' ]; then
 			op='ALTER'
 		else
 			op='CREATE'
 		fi
 
 		gosu postgres postgres --single -jE <<-EOSQL
-			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
+			$op USER "$POSTGRES_USER" WITH $pass ;
 		EOSQL
+
+		if [ "$POSTGRES_DB" != 'postgres' ]; then
+			gosu postgres postgres --single -jE <<-EOSQL
+				CREATE DATABASE "$POSTGRES_DB" WITH OWNER="$POSTGRES_USER" ENCODING='UTF8';
+			EOSQL
 		echo
-		
-		{ echo;
-      echo "host all all 0.0.0.0/0 $authMethod";
-      echo "host replication all 0.0.0.0/0 $authMethod";
+		fi
+
+		gosu postgres postgres --single -jE <<-EOSQL
+			GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_USER" TO "$POSTGRES_USER";
+		EOSQL
+
+      { echo;
+      echo "host all all 0.0.0.0/0 trust";
+      #echo "host replication all 0.0.0.0/0 trust";
+      #echo "local replication  postgres trust";
+      echo "host    replication   postgres     10.0.0.0/8 trust";
+      echo "host    replication   postgres     172.16.0.0/16 trust";
+      echo "host    replication   postgres     127.0.0.1/32 trust";
+      echo "host    replication   postgres     ::1/128      trust";
+      echo "hostnossl replication postgres     172.16.0.0/16 trust";
+      echo "hostnossl replication postgres     10.0.0.0/8 trust";
+
+      echo "host    replication   artifactory     10.0.0.0/8 trust";
+      echo "host    replication   artifactory     172.16.0.0/16 trust";
+      echo "host    replication   artifactory     127.0.0.1/32 trust";
+      echo "host    replication   artifactory     ::1/128      trust";
+      echo "hostnossl replication artifactory     172.16.0.0/16 trust";
+      echo "hostnossl replication artifactory     10.0.0.0/8 trust";
+
     } >> "$PGDATA"/pg_hba.conf
 		
 		{ echo;
@@ -79,7 +101,7 @@ if [ "$1" = 'postgres' ]; then
 		fi
 	fi
 	
-	exec gosu postgres "$@"
+	exec gosu postgres "$@" -h $HOSTNAME
 fi
 
 exec "$@"
